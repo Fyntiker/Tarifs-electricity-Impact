ee57a51816efc14fe5f640849f58d24febb280ee
Fyntiker <flament_j@hotmail.com>
2026-02-13 20:56:08 +0100

    Update config_flow.py
    Ajout des mois en lettre

--- custom_components/impact/config_flow.py
+++ custom_components/impact/config_flow.py
@@ -4,7 +4,6 @@ from homeassistant.helpers import selector
 
 from .const import DOMAIN
 
-
 GRD_OPTIONS = [
     "ORES",
     "RESa",
@@ -13,14 +12,29 @@ GRD_OPTIONS = [
     "Sibelga",
 ]
 
+MONTHS = {
+    1: "Janvier",
+    2: "Février",
+    3: "Mars",
+    4: "Avril",
+    5: "Mai",
+    6: "Juin",
+    7: "Juillet",
+    8: "Août",
+    9: "Septembre",
+    10: "Octobre",
+    11: "Novembre",
+    12: "Décembre",
+}
+
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 2
+    VERSION = 3
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(
-                title="Belgian Impact Tariff",
+                title="Tarif Impact Belgique",
                 data=user_input,
             )
 
@@ -31,20 +45,30 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required("export_entity"): selector.EntitySelector(
                 selector.EntitySelectorConfig(domain="sensor")
             ),
-            vol.Required("start_month_of_year", default=1):
-                vol.All(int, vol.Range(min=1, max=12)),
+
+            vol.Required(
+                "start_month_of_year",
+                default=1
+            ): selector.SelectSelector(
+                selector.SelectSelectorConfig(
+                    options=[
+                        {"value": k, "label": v}
+                        for k, v in MONTHS.items()
+                    ]
+                )
+            ),
 
             vol.Required("compensation_enabled", default=True): bool,
 
             vol.Required("grd", default="ORES"):
                 vol.In(GRD_OPTIONS),
 
-            # Prix transport
+            # Fallback transport manuel
             vol.Required("transport_pic", default=0.12): float,
             vol.Required("transport_medium", default=0.09): float,
             vol.Required("transport_eco", default=0.07): float,
 
-            # Prix énergie
+            # Energie
             vol.Required("energy_pic", default=0.25): float,
             vol.Required("energy_medium", default=0.22): float,
             vol.Required("energy_eco", default=0.18): float,
