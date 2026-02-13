ce616473b5970dc684b99eef93b8711343709cb1
Fyntiker <flament_j@hotmail.com>
2026-02-13 15:10:19 +0100

    Create config_flow.py

---
+++ custom_components/impact/config_flow.py
@@ -0,0 +1,22 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN, CONF_IMPORT_ENTITY, CONF_EXPORT_ENTITY
+
+class ImpactConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Impact Tariff",
+                data=user_input
+            )
+
+        schema = vol.Schema({
+            vol.Required(CONF_IMPORT_ENTITY): str,
+            vol.Optional(CONF_EXPORT_ENTITY): str,
+        })
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema
+        )
