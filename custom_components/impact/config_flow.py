1830234ae34dace7186125a511c8ef1e9fa74562
Fyntiker <flament_j@hotmail.com>
2026-02-13 16:13:58 +0100

    Update config_flow.py

--- custom_components/impact/config_flow.py
+++ custom_components/impact/config_flow.py
@@ -3,7 +3,8 @@ from homeassistant import config_entries
 from .const import DOMAIN, CONF_IMPORT_ENTITY, CONF_EXPORT_ENTITY
 
 class ImpactConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-
+    VERSION = 1
+    
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(
